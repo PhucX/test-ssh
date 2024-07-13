441cdef615987779a54abe83208a1fa504b1f241
vothnha26 <vothanhnha26@gmail.com>
2024-07-13 16:33:52 +0700

    57564

--- set2.cs
+++ set2.cs
@@ -8,7 +8,7 @@ namespace set2
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
-            //dcmoiqwkasdldajshsdn 328723
+            //dc7477 12
             Program basic = new Program();
             int[] XXX = Program.arr;
             basic.Sieve();
