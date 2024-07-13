7c62aaeb7b264535c78682a2a70f21e606f4be0b
Phuc <phuca6k5@gmail.com>
2024-07-13 16:30:19 +0700

    update code

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using bai1;
+
+namespace bai1
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            
+            Sieve sieve = new Sieve();
+            sieve.isprime();
+            int[] array = Sieve.arr;
+            
+            for (int i = 0; i < a; i++)
+            {
+                if (array[i] == 1)
+                {
+                    Console.WriteLine(i + " ");
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sieve.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    internal class Sieve
+    {
+        public static int[] arr = new int[1000];
+        public void isprime()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = 1;
+            }
+            arr[0] = 0; arr[1] = 0;
+            for (int i = 2; i < arr.Length; i++)
+            {
+                if (arr[i] == 1)
+                {
+                    for (int j = i * i; j < arr.Length; j += i)
+                    {
+                        arr[j] = 0;
+                    }
+                }
+            }
+        }
+    }
+}
