c1479a4d9c64c96014fda47e5a36e59bd1568b23
vothnha26 <vothanhnha26@gmail.com>
2024-07-13 16:16:25 +0700

    updateeee

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    public class Program
+    {
+        public static int[] arr = new int[1001];
+        public void Sieve()
+        {
+            for(int i = 0; i < arr.Length;i++) arr[i] = 1;
+            arr[0] = arr[1] = 0;
+
+            for(int i = 2;i<=Math.Sqrt(1000);i++)
+            {
+                if (arr[i] == 1)
+                {
+                    for (int j = i * i; j <= 1000; j += i) arr[j] = 0;
+                }
+            }
+        }
+
+        public void IsPrime()
+        {
+            Sieve();
+            for(int i = 0 ; i<arr.Length;i++)
+            {
+                if (arr[i] == 1) Console.Write(i + " ");
+            }
+        }
+    }
+}
---
+++ set2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using bai1;
+
+namespace set2
+{
+    class set2
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Program basic = new Program();
+            int[] XXX = Program.arr;
+            basic.Sieve();
+
+            for (int i = 0; i <= n; i++)
+            {
+                if (XXX[i] == 1) Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
